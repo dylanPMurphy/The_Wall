f63fd3d9c3e0e75c6eccfc21fc3b17d0890d239f
dylanPMurphy <dylanmurphydba@gmail.com>
2020-10-16 12:44:26 -0700

    fixed session issue

--- the_wall/the_wall_app/models.py
+++ the_wall/the_wall_app/models.py
@@ -12,7 +12,6 @@ class Message(models.Model):
 class Comment(models.Model):
     message = models.ForeignKey(Message, related_name="comments", on_delete = models.CASCADE)
     user = models.ForeignKey(User, related_name="comments", on_delete = models.CASCADE)
-
     comment = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
--- the_wall/the_wall_app/views.py
+++ the_wall/the_wall_app/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from .models import *
-
+from login_reg.models import *
 # Create your views here.
 def index(request):
     context ={
         'authenticated_user': User.objects.get(id=request.session['userid'])
-        
     }
-    return render(request, 'wall.html')
+    print(request.session)
+    print(request.session['userid'])
+    return render(request, 'wall.html', context)
